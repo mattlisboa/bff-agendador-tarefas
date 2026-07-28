86a332c928183bef217ac966ec62c18df0612198
Matheus <matheusepilef@hotmail.com>
2026-07-28 19:43:45 -0300

    Adicionado Cron

--- src/main/java/com/matheus/bffagendadortarefas/insfrastructure/client/EmailClient.java
+++ src/main/java/com/matheus/bffagendadortarefas/insfrastructure/client/EmailClient.java
@@ -3,10 +3,12 @@ package com.matheus.bffagendadortarefas.insfrastructure.client;
 
 import com.matheus.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviarEmail (@RequestBody TarefasDTOResponse dto);
 }
